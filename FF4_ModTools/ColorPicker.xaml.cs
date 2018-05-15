55f1e1d6ad915c0189dbd659325a9427d86eb1a0
Enfyve <8909014+Enfyve@users.noreply.github.com>
2018-05-15 10:51:53 -0400

    added simple color picker popup

---
+++ FF4_ModTools/ColorPicker.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FF4_ModTools
+{
+    /// <summary>
+    /// Interaction logic for ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : Window
+    {
+        public ColorPicker()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ((Canvas)sender).CaptureMouse();
+
+            Point p = e.GetPosition((Canvas)sender);
+            p.Offset(-6, -6);
+
+            byte v = (byte)(p.X / 2 + p.Y > 128 ? 255 : 0);
+            
+            Canvas.SetTop(ColorSelection, p.Y);
+            Canvas.SetLeft(ColorSelection, p.X);
+
+            ColorSelection.Stroke.SetCurrentValue(SolidColorBrush.ColorProperty, Color.FromRgb(v, v, v));
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+            {
+                ((Canvas)sender).ReleaseMouseCapture();
+                return;
+            }
+
+            Point p = e.GetPosition((Canvas)sender);
+
+            p.X = Math.Min(255, Math.Max(0, p.X)); // Clamp position
+            p.Y = Math.Min(255, Math.Max(0, p.Y)); // Clamp position
+
+            p.Offset(-6, -6); // Align center with mouse
+
+            byte v = (byte)(p.X/2 + p.Y > 128 ? 255 : 0);
+
+            Canvas.SetTop(ColorSelection, p.Y);
+            Canvas.SetLeft(ColorSelection, p.X);
+
+            ColorSelection.Stroke.SetCurrentValue(SolidColorBrush.ColorProperty,  Color.FromRgb(v, v, v));
+        }
+    }
+}
---
+++ FF4_ModTools/Resources/HueColorConverter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FF4_ModTools.Resources
+{
+    [ValueConversion(typeof(double), typeof(Color))]
+    public class HueColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var h = (double)value / 60.0;
+            int i = (int)Math.Floor(h);
+
+            double f = h - i; // Get fraction
+
+            double j = (1 - f);
+
+            byte R, G, B;
+
+            switch (i)
+            {
+                case 0:
+                    R = 255;
+                    G = (byte)Math.Round(f * 255, MidpointRounding.AwayFromZero);
+                    B = 0;
+                    break;
+                case 1:
+                    R = (byte)Math.Round(j * 255, MidpointRounding.AwayFromZero);
+                    G = 255;
+                    B = 0;
+                    break;
+                case 2:
+                    R = 0;
+                    G = 255;
+                    B = (byte)Math.Round(f * 255, MidpointRounding.AwayFromZero);
+                    break;
+                case 3:
+                    R = 0;
+                    G = (byte)Math.Round(j * 255, MidpointRounding.AwayFromZero);
+                    B = 255;
+                    break;
+                case 4:
+                    R = (byte)Math.Round(f * 255, MidpointRounding.AwayFromZero);
+                    G = 0;
+                    B = 255;
+                    break;
+                default: // 5
+                    R = 255;
+                    G = 0;
+                    B = (byte)Math.Round(j * 255, MidpointRounding.AwayFromZero);
+                    break;
+            }
+
+            return Color.FromRgb(R, G, B);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color color = (Color)value;
+
+            double R = color.R / 255d;
+            double G = color.R / 255d;
+            double B = color.R / 255d;
+
+            double Hue;
+
+            var max = Math.Max(R, Math.Max(G, B));
+            var min = Math.Min(R, Math.Min(G, B));
+            var delta = max - min;
+
+            if ((int)delta == 0) // saturation is 0, so hue is irrelevant 
+                return 0;
+
+            if (max == R)
+                Hue = (G - B) / delta;
+            else if (max == G)
+                Hue = 2 + (B - R) / delta;
+            else
+                Hue = 4 + (R - G) / delta;
+
+            return Hue * 60;
+        }
+    }
+}
